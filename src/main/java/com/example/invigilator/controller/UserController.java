c45f75a8cf73a77b3060fac80569f85784bf077e
wxy <1953179027@qq.com>
2020-10-02 10:54:19 +0800

    基本完成用户注册接口 (2.0：完善用户注册controller传参)

--- src/main/java/com/example/invigilator/controller/UserController.java
+++ src/main/java/com/example/invigilator/controller/UserController.java
@@ -19,12 +19,8 @@ public class UserController {
     @Resource
     private UserService userService;
     //用户注册
-    @RequestMapping(value = "/insert",method = RequestMethod.POST)
-    public Result insertUser(@RequestBody User user){
-        //角色id默认1
-        int roleId = 1;
-        //学校id默认1
-        int schoolId =1;
+    @RequestMapping(value = "/insert/{roleId}/{schoolId}",method = RequestMethod.POST)
+    public Result insertUser(@RequestBody User user,@PathVariable Integer roleId,@PathVariable Integer schoolId){
         int result = userService.insertUser(user,roleId,schoolId);
         if (result==1){
             return Result.success("注册成功");
